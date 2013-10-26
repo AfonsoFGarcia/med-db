7be218cdacfafe38f6ba7b65041d7bca38288076
Afonso Garcia <afonsogarcia@icloud.com>
2013-10-26 19:38:37 +0100

    Adicionado enumerado EspecialidadeType

---
+++ src/main/java/pt/ist/sirs/domain/enums/EspecialidadeType.java
@@ -0,0 +1,14 @@
+package pt.ist.sirs.domain.enums;
+
+/**
+ * Enumumerado <b>EspecialidadeType</b>. <br>
+ * <br>
+ * Descreve a especialidade do médico. <br>
+ * <br>
+ * Utilizado para verificações de permissões.
+ * 
+ * @author Afonso F. Garcia
+ */
+public enum EspecialidadeType {
+
+}
