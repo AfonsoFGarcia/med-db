ecad1eb166dc3f28a9c126dceec35a8b5e723315
Afonso Garcia <afonsogarcia@icloud.com>
2013-11-28 15:12:08 +0000

    Adicionada classe de inicialização da BD com dados

---
+++ src/main/java/pt/ist/sirs/application/PopulateDB.java
@@ -0,0 +1,24 @@
+package pt.ist.sirs.application;
+
+import pt.ist.sirs.Bootstrap;
+
+/**
+ * Classe <b>MedDBApp</b>. <br>
+ * <br>
+ * Implementa a interface de utilizador do Med-DB.
+ * 
+ * @author Afonso F. Garcia (70001)
+ */
+public class PopulateDB {
+
+    /**
+     * Main da aplicação
+     * 
+     * @param args Não utilizado
+     */
+    public static void main(String[] args) {
+        Bootstrap.init();
+
+        //Objectos a adicionar a DB no inicio
+    }
+}
