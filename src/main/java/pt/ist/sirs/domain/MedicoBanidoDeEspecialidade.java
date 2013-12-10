b3dba02dd3769be345f0c5c051665e60e83f48b1
Afonso Garcia <afonsogarcia@icloud.com>
2013-12-10 19:53:39 +0000

    Dominio modificado para ter utilizadores com poderes de admin e permitir banir medicos do acesso a uma especialidade

---
+++ src/main/java/pt/ist/sirs/domain/MedicoBanidoDeEspecialidade.java
@@ -0,0 +1,9 @@
+package pt.ist.sirs.domain;
+
+public class MedicoBanidoDeEspecialidade extends MedicoBanidoDeEspecialidade_Base {
+    
+    public  MedicoBanidoDeEspecialidade() {
+        super();
+    }
+    
+}
