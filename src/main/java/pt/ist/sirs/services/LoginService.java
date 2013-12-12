32e13e682fb6b57865c4c9067df495fd409e204d
Jose <joseldgois@hotmail.com>
2013-12-12 21:42:28 +0000

    Excepçao login pessoa tratada

--- src/main/java/pt/ist/sirs/services/LoginService.java
+++ src/main/java/pt/ist/sirs/services/LoginService.java
@@ -2,6 +2,8 @@ package pt.ist.sirs.services;
 
 import pt.ist.fenixframework.FenixFramework;
 import pt.ist.sirs.domain.MedDBRoot;
+import pt.ist.sirs.domain.Pessoa;
+import pt.ist.sirs.exceptions.IncorrectPasswordException;
 import pt.ist.sirs.exceptions.MedDBException;
 import pt.ist.sirs.login.LoggedPerson;
 
@@ -23,7 +25,13 @@ public class LoginService extends MedDBService {
     @Override
     public void run() throws MedDBException {
         MedDBRoot root = (MedDBRoot) FenixFramework.getRoot();
-        LoggedPerson.getInstance().setLoggedPerson(root.getPersonByUsername(username), password);
+        Pessoa p = new Pessoa();
+        try {
+            p = root.getPersonByUsername(username);
+        } catch (Exception e) {
+            throw new IncorrectPasswordException();
+        }
+        LoggedPerson.getInstance().setLoggedPerson(p, password);
         this.nome = LoggedPerson.getInstance().getLoggedPerson().getNome();
     }
 
