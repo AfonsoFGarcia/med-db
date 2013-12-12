f2ba65b6f703d764745d8d0330bd81ab8011017c
Jose <joseldgois@hotmail.com>
2013-12-12 21:52:25 +0000

    Excepçao login admin tratada

--- src/main/java/pt/ist/sirs/services/AdminLoginService.java
+++ src/main/java/pt/ist/sirs/services/AdminLoginService.java
@@ -3,6 +3,7 @@ package pt.ist.sirs.services;
 import pt.ist.fenixframework.FenixFramework;
 import pt.ist.sirs.domain.MedDBRoot;
 import pt.ist.sirs.domain.Pessoa;
+import pt.ist.sirs.exceptions.IncorrectPasswordException;
 import pt.ist.sirs.exceptions.MedDBException;
 import pt.ist.sirs.exceptions.NotAdminException;
 import pt.ist.sirs.login.LoggedPerson;
@@ -25,7 +26,14 @@ public class AdminLoginService extends MedDBService {
     @Override
     public void run() throws MedDBException {
         MedDBRoot root = (MedDBRoot) FenixFramework.getRoot();
-        Pessoa person = root.getPersonByUsername(username);
+        Pessoa person = new Pessoa();
+        try {
+            person = root.getPersonByUsername(username);
+        } catch (Exception e) {
+            throw new IncorrectPasswordException();
+
+        }
+
         if (person.getAdmin()) {
             LoggedPerson.getInstance().setLoggedPerson(root.getPersonByUsername(username), password);
             this.nome = LoggedPerson.getInstance().getLoggedPerson().getNome();
