23964f2a8f4eea3067330c1db759dee0fdee9521
Afonso F. Garcia <afonsogarcia@icloud.com>
2013-12-12 12:10:27 +0000

    Adicionado servico para obter registos da pessoa com login feito

---
+++ src/main/java/pt/ist/sirs/services/RegistosPropriosService.java
@@ -0,0 +1,28 @@
+package pt.ist.sirs.services;
+
+import java.util.ArrayList;
+
+import pt.ist.sirs.exceptions.MedDBException;
+import pt.ist.sirs.login.LoggedPerson;
+import pt.ist.sirs.services.dto.RegistoDTO;
+
+/**
+ * 
+ * @author Afonso F. Garcia (70001)
+ */
+public class RegistosPropriosService extends MedDBService {
+
+    private RegistosFromPacienteService recServ;
+
+    @Override
+    public void run() throws MedDBException {
+        String username = LoggedPerson.getInstance().getLoggedPerson().getUsername();
+        recServ = new RegistosFromPacienteService(username);
+        recServ.run();
+    }
+
+    public ArrayList<RegistoDTO> getRegistos() {
+        return this.recServ.getRegistos();
+    }
+
+}
